531dbdd60960a0a80b8f1b76ff7fbe452dd9b918
SebastianHernandez <sebashersan@gmail.com>
2023-10-23 11:49:52 -0500

    Add floating whatsapp

--- src/layout/LayoutPublic.jsx
+++ src/layout/LayoutPublic.jsx
@@ -2,12 +2,21 @@ import React from 'react'
 import { Header } from '../components/Header'
 import { Outlet, ScrollRestoration } from 'react-router-dom'
 import { Footer } from '../components/Footer'
+import { FloatingWhatsApp } from 'react-floating-whatsapp'
+import logo from '../assets/CSUYOPAL.jpg'
 
 export const LayoutPublic = () => {
   return (
     <div>
       <Header />
       <Outlet />
+      <FloatingWhatsApp 
+        accountName='CSU-Yopal Areandina'
+        phoneNumber='573208752152'
+        avatar={logo}
+        chatMessage={`Hola  🤝 \n¿Cómo te podemos ayudar?`}
+        allowClickAway = {true}
+      />
       <Footer />
       <ScrollRestoration />
     </div>
