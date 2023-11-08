247ee17f0baa974fcdc6fa66859bda5245323d2d
SebastianHernandez <sebashersan@gmail.com>
2023-11-08 09:48:32 -0500

    Adding faqs -Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -14,47 +14,57 @@ const Contact = () => {
       </div>
       <div className=' container-accordion '>
         <h1 className='title-faqs'>Preguntas Frecuentes</h1>
-        <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMore />}
-          >
-            <Typography>Primera Pregunta</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-              malesuada lacus ex, sit amet blandit leo lobortis eget.
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMore />}
-          >
-            <Typography>Segunda Pregunta</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-              malesuada lacus ex, sit amet blandit leo lobortis eget.
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMore />}
-          >
-            <Typography>Tercera Pregunta</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-              malesuada lacus ex, sit amet blandit leo lobortis eget.
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
+        {Faqs.map((faq, index) => {
+          return (
+            <Accordion key={index}>
+              <AccordionSummary
+                expandIcon={<ExpandMore />}
+              >
+                <Typography>{ faq.title }</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>{ faq.resp }</Typography>
+              </AccordionDetails>
+            </Accordion>
+          )
+        })}
       </div>
     </div>
   )
 }
 export default Contact
+
+const Faqs = [
+  {
+    title: '¿Que modalidad es? ',
+    resp: '100% virtual'
+  },
+  {
+    title: 'Sedes de la universidad',
+    resp: '-Bogotá \n-Pereira \n-Valledupar \nRecuerda que la modalidad es virtual y puedes estudiar desde cualquier lugar.'
+  },
+  {
+    title: 'Modalidad de pago',
+    resp: '-Contado \n-Cesantías \n-Crédito'
+  },
+  {
+    title: '¿La universidad está acreditada o certificada?',
+    resp: 'La universidad cuenta con acreditación multicampus'
+  },
+  {
+    title: 'Requisitoos de inscripción',
+    resp: 'Para iniciar con el proceso solo son requeridos los siguientes documentos:\n-Documento de identidad \n-Diploma bachiller \n-Icfes \n-Dirección y ciudad de residencia. \n-Correo electrónico personal'
+  },
+  {
+    title: 'Convenios',
+    resp: '-Fuerzas militares \n-Coomeva \n-Sena \nEntre otros...'
+  },
+  {
+    title: '¿Los títulos Sena son homologables? ',
+    resp: 'Sí...'
+  },
+  {
+    title: '¿Tienen posgrados? ',
+    resp: 'Sí...\n-Esp. en Auditoria en Salud...$ 7.260.000 \n-Esp. en Informática Educativa...$ 3.490.000 \n-Esp. en Pedagogía y Docencia...$ 3.540.000\n-Esp. Gerencia en Seguridad y Salud en el Trabajo...$ 7.060.000\n-Esp. Gestión Ambiental...$ 3.660.000\n-Esp. Gerencia Financiera...$ 5.130.000\n-Esp. Alta Gerencia...$ 5.190.000\n-Esp. En dercho de autor, propiedad industrial y nuevas tecnologias...$ 4.350.000\n-Maestria en Gerencia de Mercadeo Digital...$ 5.840.000\n-Maestria en Epidemiologia...$ 7.830.000\n-Maestria en Innovación...$ 6.630.000'
+  }
+]
